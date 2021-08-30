2c0cfb6942ae5979d1c262b339e7c3c2e121e46d
nmolby <molbynathan1@gmail.com>
2021-08-30 11:15:22 -0400

    Initial

---
+++ FantasyHacker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FantasyHacker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
