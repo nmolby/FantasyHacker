7fd2171dfc5ada5614b7c6595f64e9be323d4267
nmolby <molbynathan1@gmail.com>
2021-09-13 12:00:18 -0400

    Created  base classes, implemented scoring functions.

--- FantasyHacker/Program.cs
+++ FantasyHacker/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text.Json;
+using FantasyHacker;
+
 
 namespace FantasyHacker
 {
@@ -6,7 +9,16 @@ namespace FantasyHacker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string myJsonResponse = System.IO.File.ReadAllText(@"/Users/nmolby/Projects/FantasyHacker/FantasyHacker/BoxScore.json");
+            BoxScore.BoxScore boxScore = JsonSerializer.Deserialize<BoxScore.BoxScore>(myJsonResponse);
+
+            foreach(Interface.IFantasyPlayer player in boxScore.Score())
+            {
+                if(player.Score() != 0)
+                {
+                    Console.WriteLine($"Player {player.Person.FullName} : {player.Score()}");
+                }
+            }
         }
     }
 }
